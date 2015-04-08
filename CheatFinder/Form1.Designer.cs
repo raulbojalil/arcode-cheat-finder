e5b558b03b12d2c7e971fefd542826259e1a0c19
Raul Bojalil <raulbojalil@gmail.com>
2015-04-07 19:47:06 -0500

    First commit

---
+++ CheatFinder/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace CheatFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCheatCode = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBrowseDumps = new System.Windows.Forms.Button();
+            this.txtDumps = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtValues = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tslbStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tspbProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.nudNewValue = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNewValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCheatCode
+            // 
+            this.txtCheatCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCheatCode.Location = new System.Drawing.Point(12, 21);
+            this.txtCheatCode.Name = "txtCheatCode";
+            this.txtCheatCode.ReadOnly = true;
+            this.txtCheatCode.Size = new System.Drawing.Size(441, 20);
+            this.txtCheatCode.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnBrowseDumps);
+            this.groupBox1.Controls.Add(this.txtDumps);
+            this.groupBox1.Location = new System.Drawing.Point(18, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(273, 278);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FCRAM Dumps (one per line)";
+            // 
+            // btnBrowseDumps
+            // 
+            this.btnBrowseDumps.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseDumps.Location = new System.Drawing.Point(21, 19);
+            this.btnBrowseDumps.Name = "btnBrowseDumps";
+            this.btnBrowseDumps.Size = new System.Drawing.Size(235, 23);
+            this.btnBrowseDumps.TabIndex = 1;
+            this.btnBrowseDumps.Text = "Browse...";
+            this.btnBrowseDumps.UseVisualStyleBackColor = true;
+            this.btnBrowseDumps.Click += new System.EventHandler(this.btnBrowseDumps_Click);
+            // 
+            // txtDumps
+            // 
+            this.txtDumps.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDumps.Location = new System.Drawing.Point(21, 48);
+            this.txtDumps.Multiline = true;
+            this.txtDumps.Name = "txtDumps";
+            this.txtDumps.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDumps.Size = new System.Drawing.Size(235, 211);
+            this.txtDumps.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtValues);
+            this.groupBox2.Location = new System.Drawing.Point(305, 36);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(257, 278);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Values to find (one per line per dump file, ordered)";
+            // 
+            // txtValues
+            // 
+            this.txtValues.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtValues.Location = new System.Drawing.Point(16, 48);
+            this.txtValues.Multiline = true;
+            this.txtValues.Name = "txtValues";
+            this.txtValues.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtValues.Size = new System.Drawing.Size(221, 211);
+            this.txtValues.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.btnGenerate);
+            this.groupBox3.Controls.Add(this.txtCheatCode);
+            this.groupBox3.Location = new System.Drawing.Point(18, 381);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(544, 51);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resulting cheat code";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerate.Location = new System.Drawing.Point(459, 19);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "GENERATE";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(579, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.aboutToolStripMenuItem.Text = "&About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslbStatus,
+            this.toolStripStatusLabel1,
+            this.tspbProgress});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 441);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(579, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tslbStatus
+            // 
+            this.tslbStatus.Name = "tslbStatus";
+            this.tslbStatus.Size = new System.Drawing.Size(39, 17);
+            this.tslbStatus.Text = "Ready";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(423, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            // 
+            // tspbProgress
+            // 
+            this.tspbProgress.Name = "tspbProgress";
+            this.tspbProgress.Size = new System.Drawing.Size(100, 16);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.Controls.Add(this.nudNewValue);
+            this.groupBox4.Location = new System.Drawing.Point(18, 320);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(544, 55);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "New Value";
+            // 
+            // nudNewValue
+            // 
+            this.nudNewValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudNewValue.Location = new System.Drawing.Point(12, 23);
+            this.nudNewValue.Maximum = new decimal(new int[] {
+            -1,
+            0,
+            0,
+            0});
+            this.nudNewValue.Name = "nudNewValue";
+            this.nudNewValue.Size = new System.Drawing.Size(522, 20);
+            this.nudNewValue.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 463);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ARCode Cheat Finder v0.1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNewValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCheatCode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBrowseDumps;
+        private System.Windows.Forms.TextBox txtDumps;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtValues;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tslbStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar tspbProgress;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.NumericUpDown nudNewValue;
+    }
+}
+
---
+++ CheatFinder/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CheatFinder
+{
+    public partial class Form1 : Form
+    {
+        public string GenerateCheatCode(string[] dumps, string[] values, long newValue)
+        {
+            if (dumps == null || dumps.Length == 0)
+                throw new ArgumentException("At least one dump file is required.");
+
+            if (values == null || values.Length == 0)
+                throw new ArgumentException("At least one value is required (one for each dump file).");
+
+            List<byte[]> dumpData = new List<byte[]>();
+            List<int[]> valuesSwapped = new List<int[]>(); 
+
+            foreach (var dump in dumps)
+            {
+                if(!string.IsNullOrEmpty(dump))
+                dumpData.Add(File.ReadAllBytes(dump));
+            }
+
+            if(dumpData.Count == 0)
+                throw new ArgumentException("At least one dump file is required.");
+
+            var valueLength = -1;
+            var max = -1;
+            foreach (var value in values)
+            {
+                long valueAsLong = 0;
+                if (!Int64.TryParse(value, out valueAsLong))
+                    throw new Exception(string.Format("'{0}' is an invalid value. Values should be decimal numbers.", value));
+
+                valuesSwapped.Add(SwapEndianness(valueAsLong, out max));
+                if (max > valueLength)
+                    valueLength = max;
+            }
+
+            if (dumpData.Count != valuesSwapped.Count)
+                throw new ArgumentException("Please specify a value (decimal) for each dump file.");
+
+            var result = string.Empty;
+
+            for (int i = 0; i < dumpData[0].Length; i++)
+            {
+                var offset = 0;
+                foreach (var firstValueByte in valuesSwapped[0])
+                {
+                    if (i + offset < dumpData[0].Length && dumpData[0][i + offset] == firstValueByte)
+                    {
+                        if (offset == valueLength - 1)
+                        {
+                            var found = true;
+                            if (dumpData.Count > 1)
+                                found = SearchValuesAtAddress(i, valueLength, dumpData, valuesSwapped, 1);
+
+                            if (found)
+                            {
+                                if (!string.IsNullOrEmpty(result))
+                                    throw new Exception("More than one address with the specified value(s) has been found, please narrow down your search and/or verify the input data.");
+
+                                result = string.Format("{0:X8} {1:X8}", i, newValue);
+                            }
+
+                        }
+                    }
+                    else
+                        break;
+
+                    offset++;
+                }
+            }
+
+            return result;
+
+            
+
+        }
+
+        private bool SearchValuesAtAddress(int address, int valueLength, List<byte[]> dumpData, List<int[]> values, int pos)
+        {
+            if (pos == dumpData.Count)
+                return true;
+
+            for (var i = 0; i < valueLength; i++ )
+            {
+                if (values[pos][i] == dumpData[pos][address + i])
+                {
+                    if (i == valueLength - 1)
+                        return SearchValuesAtAddress(address, valueLength, dumpData, values, pos + 1);
+                }
+                else 
+                    return false;
+            }
+            return false;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            List<int> diffs = new List<int>();
+
+            var data1 = File.ReadAllBytes("C:\\FCRAM-19.25.bin");
+            var data2 = File.ReadAllBytes("C:\\FCRAM-19.22.bin");
+
+            for (int i = 0; i < data1.Length; i++)
+            {
+                if(data1[i] != data2[i])
+                {
+                    diffs.Add(i);
+                }
+            }
+
+        }
+
+
+
+
+        public static int[] SwapEndianness(long value, out int length)
+        {
+            var array = new int[] { ((int)value & 0xFF), ((int)value & 0xFF00) >> 8, ((int)value & 0xFF0000) >> 16, (int)((value & 0xFF000000) >> 24) };
+            length = (array[2] == 0 && array[3] == 0) ? 2 : 4;
+            return array;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+            EnableControls(false, "Generating cheat code, please wait...");
+
+            backgroundWorker1.RunWorkerAsync(new object[] {
+                
+                txtDumps.Lines,
+                txtValues.Lines,
+                (long)nudNewValue.Value
+            });
+
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBrowseDumps_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Please select one or more dumps";
+            openFileDialog.Filter = "FCRAM Dump file|*.bin";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StringBuilder builder = new StringBuilder();
+                var index = 0;
+                foreach (var filename in openFileDialog.FileNames)
+                {
+                    if (index > 0)
+                        builder.Append("\r\n");
+                    builder.AppendFormat("{0}", filename);
+                    index++;
+                }
+                txtDumps.Text = builder.ToString();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var arguments = e.Argument as object[];
+            e.Result = GenerateCheatCode(
+                arguments[0] as string[], 
+                arguments[1] as string[], 
+                Convert.ToInt64(arguments[2]));
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                EnableControls(true, e.Error.Message);
+            }
+            else
+            {
+                txtCheatCode.Text = e.Result.ToString();
+                if (!string.IsNullOrEmpty(txtCheatCode.Text))
+                {
+                    Clipboard.SetText(txtCheatCode.Text);
+                    EnableControls(true, "Cheat code generated successfully.");
+                    MessageBox.Show("The cheat code has been copied to your clipboard.", "Cheat code generated successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    EnableControls(true, "Address not found.");
+                    MessageBox.Show("No addresses with the specified values were found. Please try again.", "Address not found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                }
+            }
+            
+        }
+
+        public void EnableControls(bool enabled, string status)
+        {
+            menuStrip1.Enabled = enabled;
+            groupBox1.Enabled = enabled;
+            groupBox2.Enabled = enabled;
+            groupBox3.Enabled = enabled;
+            groupBox4.Enabled = enabled;
+
+            tslbStatus.Text = status;
+            tspbProgress.Style = enabled ? ProgressBarStyle.Blocks : ProgressBarStyle.Marquee;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Raúl Bojalil \r\nhttp://raulbojalil.com\r\n\r\nMany thanks to the creators of ARCode. For more information please go to:\r\nhttp://gbatemp.net/threads/spider-arcode.383937/", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+         
+        }
+
+    }
+}
